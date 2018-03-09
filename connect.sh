b97f3d20678c4f78e72cc5a1fa97b11f5de90b6f
coaxial <hi@64b.it>
2018-03-09 16:20:15 -0500

    Allow for arbitrary commands

--- connect.sh
+++ connect.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker-compose exec database psql -h database -U postgres
+# Will run supplied command in the database container. Default command is to
+# connect to the local database with psql
+COMMAND="${1:-psql -h localhost -U postgres}"
+
+docker-compose exec database $COMMAND
