07417f97383284b09b4e8101604d56e1fd0374cc
coaxial <hi@64b.it>
2018-03-13 13:56:54 -0400

    Add materialized view for enceladus events
    Views are a way to turn queries into functions, so you don't have to retype the full query every time; only supply the variables
    Materialized views are snapshot views in a way, kinda like cached views that have to me manually refreshed when new data comes in

---
+++ sql/0006-materialized-view.sql
@@ -0,0 +1,13 @@
+drop materialized view if exists enceladus_events;
+
+create materialized view enceladus_events
+as select events.id, events.title, events.description, event_types.description as event, events.time_stamp::date as date, events.time_stamp, to_tsvector(concat(events.description, ' ', events.title)) as search
+from events
+inner join event_types on event_types.id=events.event_type_id
+where target_id=28
+order by time_stamp;
+
+create index idx_event_search on enceladus_events using GIN(search);
+
+-- query it with
+-- select * from enceladus_events where date='2005-02-17';
